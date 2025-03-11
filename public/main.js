a687c63d34e38d8fe61f269ee502182db161a0d6
JM <maricj005@gmail.com>
2025-03-11 14:00:21 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
-// Selektovanje strelica i skupova karaktera
-const rightArrow = document.getElementById('arrow');  // Desna strelica
-const leftArrow = document.getElementById('arrow1');  // Leva strelica
 
-// Prvi set karaktera
+const rightArrow = document.getElementById('arrow');  
+const leftArrow = document.getElementById('arrow1');  
+
+
 const firstSetCharacters = [
   document.getElementById('character1'),
   document.getElementById('character2'),
@@ -10,7 +10,7 @@ const firstSetCharacters = [
   document.getElementById('character4')
 ];
 
-// Drugi set karaktera
+
 const secondSetCharacters = [
   document.getElementById('character5'),
   document.getElementById('character6'),
@@ -18,59 +18,55 @@ const secondSetCharacters = [
   document.getElementById('character8')
 ];
 
-// Inicijalne postavke
+
 secondSetCharacters.forEach(function(character) {
-  character.style.display = 'none'; // Sakrij drugi set na početku
+  character.style.display = 'none'; 
 });
-leftArrow.style.opacity = '0.25'; // Leva strelica je neaktivna
+leftArrow.style.opacity = '0.25'; 
 leftArrow.style.cursor = 'default';
 
-// Funkcija za prikazivanje prvog seta
+
 function showFirstSet() {
-  // Sakrij drugi set
+  
   secondSetCharacters.forEach(function(character) {
     character.style.display = 'none';
   });
 
-  // Prikaži prvi set
+  
   firstSetCharacters.forEach(function(character) {
     character.style.display = 'flex';
   });
 
-  // Ažuriraj strelice
+  
   leftArrow.style.opacity = '0.25';
   leftArrow.style.cursor = 'default';
   rightArrow.style.opacity = '1';
   rightArrow.style.cursor = 'pointer';
 }
 
-// Funkcija za prikazivanje drugog seta
+
 function showSecondSet() {
-  // Sakrij prvi set
+  
   firstSetCharacters.forEach(function(character) {
     character.style.display = 'none';
   });
 
-  // Prikaži drugi set
   secondSetCharacters.forEach(function(character) {
     character.style.display = 'flex';
   });
 
-  // Ažuriraj strelice
   rightArrow.style.opacity = '0.25';
   rightArrow.style.cursor = 'default';
   leftArrow.style.opacity = '1';
   leftArrow.style.cursor = 'pointer';
 }
 
-// Event listener za desnu strelicu
 rightArrow.addEventListener('click', function() {
   if (rightArrow.style.cursor !== 'default') {
     showSecondSet();
   }
 });
 
-// Event listener za levu strelicu
 leftArrow.addEventListener('click', function() {
   if (leftArrow.style.cursor !== 'default') {
     showFirstSet();
